5ba8825781674ad71b35a2e4c31a90602612cdbb
Rafael Zoucas <rafaelpzoucas@gmail.com>
2022-07-14 15:15:10 -0300

    commit brabo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { Pizza } from "phosphor-react"
+import { useState } from "react"
+import { Participant } from "./components/Participant"
+
 function App() {
+
   return (
-    <div className="App">
-      
+    <div className="App flex-grow p-4 pb-24">
+      <div className="flex flex-row pb-4 gap-4 align-middle">
+        <div className="p-2 rounded-full bg-amber-100 flex justify-center align-middle">
+            <Pizza size={28} weight="fill" className="text-red-500" />
+        </div>
+        <strong className="text-2xl">Contador para Rodizio</strong>
+      </div>
+
+      <div className="flex flex-col gap-4 overflow-auto">
+          <Participant />
+          <Participant />
+          <Participant />
+          <Participant />
+          <Participant />
+          <Participant />
+      </div>
+
+      <div className="fixed bottom-0 left-0 flex flex-grow p-4 bg-white w-full drop-shadow-2xl">
+        <button className="flex flex-grow justify-center bg-green-500 p-4 rounded-lg">+ Adicionar participante</button>
+      </div>
     </div>
   )
 }
