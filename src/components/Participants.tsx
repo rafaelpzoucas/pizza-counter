e376ef709889f49ad9fa0df0090c8900cb48d0f4
Rafael Zoucas <rafaelpzoucas@gmail.com>
2022-10-07 14:38:51 -0300

    Update Participants.tsx

--- src/components/Participants.tsx
+++ src/components/Participants.tsx
@@ -10,14 +10,16 @@ export function Participants() {
     }
 
     return (
-        <div className="flex flex-col gap-2 p-4">
-            {
-                participants.map(() => {
-                    return (
-                        <Participant />
-                    )
-                })
-            }
+        <div className="flex flex-col gap-8 p-4">
+            <main className="flex flex-col gap-2">
+                {
+                    participants.map(() => {
+                        return (
+                                <Participant />
+                        )
+                    })
+                }
+            </main>
 
             <button
                 className="flex items-center justify-center w-full rounded-lg p-4 font-bold text-zinc-100 bg-rose-700"
