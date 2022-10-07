8aa00a6c517ee88333349b35dc2fa4b8ec2b8117
Rafael Zoucas <rafaelpzoucas@gmail.com>
2022-10-07 14:33:56 -0300

    Update Participants.tsx

--- src/components/Participants.tsx
+++ src/components/Participants.tsx
@@ -19,14 +19,12 @@ export function Participants() {
                 })
             }
 
-            <div className="bg-white">
-                <button
-                    className="flex items-center justify-center w-full rounded-lg p-4 font-bold text-zinc-100 bg-rose-700"
-                    onClick={handleCreateNewParticipant}
-                >
-                    Adidionar participante
-                </button>
-            </div>
+            <button
+                className="flex items-center justify-center w-full rounded-lg p-4 font-bold text-zinc-100 bg-rose-700"
+                onClick={handleCreateNewParticipant}
+            >
+                Adidionar participante
+            </button>
         </div>
     )
 }
