1a66da790c682b1bf686f051a42c89bd92b53493
Rafael Zoucas <rafaelpzoucas@gmail.com>
2022-10-07 14:32:14 -0300

    Update Participants.tsx

--- src/components/Participants.tsx
+++ src/components/Participants.tsx
@@ -21,6 +21,7 @@ export function Participants() {
 
             <div className="bg-white">
                 <button
+                    className="flex items-center justify-center w-full rounded-lg p-4 font-bold text-zinc-100 bg-rose-700"
                     onClick={handleCreateNewParticipant}
                 >
                     Adidionar participante
