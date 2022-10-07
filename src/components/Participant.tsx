1b10d9b0fd78b4f6f8dbd71f1951a8c529c1ba24
Rafael Zoucas <rafaelpzoucas@gmail.com>
2022-10-07 14:46:46 -0300

    Update Participant.tsx

--- src/components/Participant.tsx
+++ src/components/Participant.tsx
@@ -21,7 +21,7 @@ export function Participant() {
                 <input 
                     type="text" 
                     placeholder="Digite um nome..."
-                    className="bg-transparent"
+                    className="bg-transparent focus:outline-none"
                 />
                 <strong>{count} fatias</strong>
             </header>
